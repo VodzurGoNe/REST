8e58599c2feb9075ea876573e9efb97561fca253
Vladislav Gruzdov <gruzdov.v.1991@hotmail.com>
2021-05-18 20:15:53 +0400

    Мелкие исправления и доработки

--- spring_course_rest/src/main/java/org/gruzdov/spring/rest/configuration/MyConfig.java
+++ spring_course_rest/src/main/java/org/gruzdov/spring/rest/configuration/MyConfig.java
@@ -4,9 +4,13 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 import java.beans.PropertyVetoException;
 import java.util.Properties;
 import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -16,17 +20,24 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan({"org.gruzdov.spring.rest"})
 @EnableWebMvc
 @EnableTransactionManagement
+@PropertySource(value = "classpath:db.property")
 public class MyConfig {
+    private Environment environment;
 
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
     @Bean
     public DataSource dataSource() {
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
 
         try {
-            dataSource.setDriverClass("org.postgresql.Driver");
-            dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/java_ee_db?useSSL=false");
-            dataSource.setUser("postgres");
-            dataSource.setPassword("DualSword163!@");
+            dataSource.setDriverClass(environment.getRequiredProperty("db.driverClassName"));
+            dataSource.setJdbcUrl(environment.getRequiredProperty("db.url"));
+            dataSource.setUser(environment.getRequiredProperty("db.username"));
+            dataSource.setPassword(environment.getRequiredProperty("db.password"));
+
         } catch (PropertyVetoException var3) {
             var3.printStackTrace();
         }
@@ -36,13 +47,19 @@ public class MyConfig {
 
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
+
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(this.dataSource());
         sessionFactory.setPackagesToScan(new String[]{"org.gruzdov.spring.rest.entity"});
+
         Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        hibernateProperties.setProperty("hibernate.dialect"
+                , environment.getRequiredProperty("hibernate.dialect"));
+        hibernateProperties.setProperty("hibernate.show_sql"
+                , environment.getRequiredProperty("hibernate.show_sql"));
+
         sessionFactory.setHibernateProperties(hibernateProperties);
+
         return sessionFactory;
     }
 
@@ -50,6 +67,7 @@ public class MyConfig {
     public HibernateTransactionManager transactionManager() {
         HibernateTransactionManager transactionManager = new HibernateTransactionManager();
         transactionManager.setSessionFactory(this.sessionFactory().getObject());
+
         return transactionManager;
     }
 }
